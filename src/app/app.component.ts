a9c693ac5f570bddc819d0446efbdbb381e7a525
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-10 16:16:37 -0300

    ngIf e ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
 
+import {StudentModel} from './student/student.model';
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  luke = {
-    name: 'Luke',
-    isJedi: true,
-    temple: 'Coruscant'
-  };
-  leia = {
+  students: StudentModel[] = [
+    {
+      name: 'Luke',
+      isJedi: true,
+      temple: 'Coruscant'
+    },
+    {
     name: 'Leia',
     isJedi: false,
-  };
-  han = {
-    name: 'Han Solo',
-    isJedi: false,
-  };
+    },
+    {
+      name: 'Han Solo',
+      isJedi: false,
+    }
+  ];
 }
